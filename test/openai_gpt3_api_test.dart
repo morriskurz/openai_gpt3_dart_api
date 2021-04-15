1ae06727793178edfb0117243b689a58a94e25cb
Morris Kurz <morriskurz@gmail.com>
2021-04-15 11:39:45 +0200

    Initial commit of the GPT3 API calls for search, completion and classification.

---
+++ test/openai_gpt3_api_test.dart
@@ -0,0 +1,179 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:openai_gpt3_api/openai_gpt3_api.dart';
+
+void main() {
+  GPT3? api;
+  const OPENAI_API_KEY =
+      String.fromEnvironment('OPENAI_API_KEY', defaultValue: '123123123');
+
+  setUp(() {
+    api = GPT3(OPENAI_API_KEY);
+  });
+
+  group('Search API', () {
+    test('call with documents and file throws an ArgumentError', () async {
+      expect(
+          api!.search('How to write efficient tests?',
+              documents: ['Not like this', 'Like this'], file: 'test'),
+          throwsArgumentError);
+    });
+
+    test('call without documents and file throws an ArgumentError', () async {
+      expect(api!.search('How to write efficient tests?'), throwsArgumentError);
+    });
+
+    test('ada parameter calls the ada engine', () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'], engine: Engine.ada);
+      expect(result.model.startsWith('ada'), isTrue);
+    });
+
+    test('search data is not empty', () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'], engine: Engine.ada);
+      expect(result.data.isNotEmpty, isTrue);
+    });
+
+    test('babbage parameter calls the babbage engine', () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'], engine: Engine.babbage);
+      expect(result.model.startsWith('babbage'), isTrue);
+    });
+
+    test('curie parameter calls the curie engine', () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'], engine: Engine.curie);
+      expect(result.model.startsWith('curie'), isTrue);
+    });
+
+    test('curie-instruct parameter calls the curie-instruct engine', () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'],
+          engine: Engine.curieInstruct);
+      expect(result.model.startsWith('if-curie'), isTrue);
+    });
+
+    test('davinci-instruct parameter calls the davinci-instruct engine',
+        () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'],
+          engine: Engine.davinciInstruct);
+      expect(result.model.startsWith('if-davinci'), isTrue);
+    });
+
+    test('davinci parameter calls the davinci engine', () async {
+      var result = await api!.search('How to write efficient tests?',
+          documents: ['Not like this', 'Like this'], engine: Engine.davinci);
+      expect(result.model.startsWith('davinci'), isTrue);
+    });
+
+    test('unknown file parameter throws an InvalidRequestException', () async {
+      expect(
+          () => api!.search('How to write efficient tests?',
+              file: 'Not like this', engine: Engine.babbage),
+          throwsA(isInstanceOf<InvalidRequestException>()));
+    });
+
+    test('Search API with invalid API key throws an InvalidRequestException',
+        () async {
+      api = GPT3('123123');
+      expect(
+          () => api!.search('How to write efficient tests?',
+              file: 'Not like this', engine: Engine.babbage),
+          throwsA(isInstanceOf<InvalidRequestException>()));
+    });
+  });
+
+  group('Classification API', () {
+    test('When both examples and file are given, throws an ArgumentError',
+        () async {
+      expect(
+          api!.classification(Engine.ada, 'How to write efficient tests?',
+              examples: [
+                ['Like this', 'Positive']
+              ],
+              file: 'test'),
+          throwsArgumentError);
+    });
+
+    test('When neither examples nor file are given, throws an ArgumentError',
+        () async {
+      expect(api!.classification(Engine.ada, 'How to write efficient tests?'),
+          throwsArgumentError);
+    });
+
+    test('ada parameter calls the ada engine', () async {
+      var result = await api!.classification(
+          Engine.ada, 'How to write efficient tests?',
+          examples: [
+            ['Like this', 'Positive']
+          ],
+          labels: [
+            'Positive',
+            'Negative',
+            'Neutral'
+          ]);
+      expect(result.model.startsWith('ada'), isTrue);
+    });
+
+    test('babbage parameter calls the babbage engine', () async {
+      var result = await api!.classification(
+        Engine.babbage,
+        'How to write efficient tests?',
+        examples: [
+          ['Like this', 'Positive']
+        ],
+      );
+      expect(result.model.startsWith('babbage'), isTrue);
+    });
+
+    test('unknown file parameter throws an InvalidRequestException', () async {
+      expect(
+          () => api!.classification(Engine.ada, 'How to write efficient tests?',
+              file: '123123'),
+          throwsA(isInstanceOf<InvalidRequestException>()));
+    });
+
+    test('invalid API key throws an InvalidRequestException', () async {
+      api = GPT3('123123');
+      expect(
+          () => api!.classification(
+                Engine.babbage,
+                'How to write efficient tests?',
+                examples: [
+                  ['Like this', 'Positive']
+                ],
+              ),
+          throwsA(isInstanceOf<InvalidRequestException>()));
+    });
+  });
+
+  group('Completion API', () {
+    test('ada parameter calls the ada engine', () async {
+      var result = await api!.completion('How to write efficient tests?',
+          maxTokens: 1, engine: Engine.ada);
+      expect(result.model.startsWith('ada'), isTrue);
+    });
+
+    test('babbage parameter calls the babbage engine', () async {
+      var result = await api!.completion('How to write efficient tests?',
+          maxTokens: 1, engine: Engine.babbage);
+      expect(result.model.startsWith('babbage'), isTrue);
+    });
+
+    test('logprobs are not null when the parameter logProbs is given.',
+        () async {
+      var result = await api!.completion('How to write efficient tests?',
+          maxTokens: 1, engine: Engine.babbage, logProbs: 2);
+      expect(result.choices.first.logprobs, isNotNull);
+    });
+
+    test('invalid API key throws an InvalidRequestException', () async {
+      api = GPT3('123123');
+      expect(
+          () => api!.completion('How to write efficient tests?',
+              engine: Engine.babbage),
+          throwsA(isInstanceOf<InvalidRequestException>()));
+    });
+  });
+}
